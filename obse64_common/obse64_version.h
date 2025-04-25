f93c7fcc19b579e0aac81a42190f0bb18396d800
ianpatt <ianpatt@gmail.com>
2025-04-24 17:33:48 -0700

    fix version bit mapping for virtuos version numbers

--- obse64_common/obse64_version.h
+++ obse64_common/obse64_version.h
@@ -2,18 +2,23 @@
 #define __OBSE64_VERSION_H__
 
 // these have to be macros so they can be used in the .rc
-#define OBSE_VERSION_INTEGER		    0
+#define OBSE_VERSION_INTEGER		0
 #define OBSE_VERSION_INTEGER_MINOR	1
-#define OBSE_VERSION_INTEGER_BETA     0
+#define OBSE_VERSION_INTEGER_BETA	0
 #define OBSE_VERSION_VERSTRING		"0, 0, 1, 0"
-#define OBSE_VERSION_PADDEDSTRING     "0001"
-#define OBSE_VERSION_RELEASEIDX       1
+#define OBSE_VERSION_PADDEDSTRING	"0001"
+#define OBSE_VERSION_RELEASEIDX		1
 
-#define MAKE_EXE_VERSION_EX(major, minor, build, sub)	((((major) & 0xFF) << 24) | (((minor) & 0xFF) << 16) | (((build) & 0xFFF) << 4) | ((sub) & 0xF))
+//	x-------	major
+//	-xxx----	minor
+//	----xxx-	build
+//	-------x	sub
+
+#define MAKE_EXE_VERSION_EX(major, minor, build, sub)	((((major) & 0xF) << 28) | (((minor) & 0xFFF) << 16) | (((build) & 0xFFF) << 4) | ((sub) & 0xF))
 #define MAKE_EXE_VERSION(major, minor, build)			MAKE_EXE_VERSION_EX(major, minor, build, 0)
 
-#define GET_EXE_VERSION_MAJOR(a)	(((a) & 0xFF000000) >> 24)
-#define GET_EXE_VERSION_MINOR(a)	(((a) & 0x00FF0000) >> 16)
+#define GET_EXE_VERSION_MAJOR(a)	(((a) & 0xF0000000) >> 28)
+#define GET_EXE_VERSION_MINOR(a)	(((a) & 0x0FFF0000) >> 16)
 #define GET_EXE_VERSION_BUILD(a)	(((a) & 0x0000FFF0) >> 4)
 #define GET_EXE_VERSION_SUB(a)		(((a) & 0x0000000F) >> 0)
 
@@ -22,7 +27,7 @@
 #define	RUNTIME_TYPE_EPIC		2
 #define	RUNTIME_TYPE_MSSTORE	3
 
-#define RUNTIME_VERSION_0_411_140	MAKE_EXE_VERSION(0, 411, 140)	// 0x009B08C0	initial Steam release
+#define RUNTIME_VERSION_0_411_140	MAKE_EXE_VERSION(0, 411, 140)	// 0x019B08C0	initial Steam release
 
 #define PACKED_OBSE_VERSION		MAKE_EXE_VERSION(OBSE_VERSION_INTEGER, OBSE_VERSION_INTEGER_MINOR, OBSE_VERSION_INTEGER_BETA)
 
