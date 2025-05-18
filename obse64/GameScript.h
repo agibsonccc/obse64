90705a54768599337a2a471721b344f1cf4c03a9
ianpatt <ianpatt@gmail.com>
2025-05-18 14:22:47 -0700

    move command macros to GameScript

--- obse64/GameScript.h
+++ obse64/GameScript.h
@@ -131,6 +131,11 @@ typedef bool (* _ExtractArgs)(
 	...);
 extern RelocAddr <_ExtractArgs> ExtractArgs;
 
+#define COMMAND_ARGS		const ParamInfo * paramInfo, const char * scriptData, TESObjectREFR * thisObj, TESObjectREFR * containingObj, Script * script, ScriptLocals * locals, float * result, u32 * opcodeOffsetPtr
+#define PASS_COMMAND_ARGS	paramInfo, scriptData, thisObj, containingObj, script, locals, result, opcodeOffsetPtr
+#define EXTRACT_ARGS		paramInfo, scriptData, opcodeOffsetPtr, thisObj, containingObj, script, locals
+#define COMMAND_ARGS_EVAL	TESObjectREFR * thisObj, void * arg1, void * arg2, double * result
+
 enum
 {
 	kScript_NumScriptCommands = 0x0172,
