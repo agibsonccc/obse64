edd459701a31603c0a4db9c6f36e91eef244da8f
ianpatt <ianpatt@gmail.com>
2025-05-19 20:47:43 -0700

    PlayerCharacter stubs

--- obse64/GameObjects.h
+++ obse64/GameObjects.h
@@ -6,6 +6,7 @@
 #include "AltarSync.h"
 
 class NiNode;
+class BaseProcess;
 
 // 8
 class TESChildCell
@@ -15,6 +16,8 @@ public:
 
 //	void	** _vtbl;	// 0
 };
+
+static_assert(sizeof(TESChildCell) == 0x8);
 
 // 138
 class TESObjectREFR : public TESForm
@@ -98,3 +101,206 @@ public:
 	// new in altar
 	u8	unkA8[0x138 - 0x0A8];
 };
+
+static_assert(sizeof(TESObjectREFR) == 0x138);
+
+// 148
+class MobileObject : public TESObjectREFR
+{
+public:
+	virtual ~MobileObject();
+
+	virtual void Unk_77();
+	virtual void Unk_78();
+	virtual void Unk_79();
+	virtual void Unk_7A();
+	virtual void Unk_7B();
+	virtual void Unk_7C();
+	virtual void Unk_7D();
+	virtual void Unk_7E();
+	virtual void Unk_7F();
+	virtual void Unk_80();
+	virtual void Unk_81();
+	virtual void Unk_82();
+	virtual void Unk_83();
+	virtual void Unk_84();
+	virtual void Unk_85();
+	virtual void Unk_86();
+	virtual void Unk_87();
+	virtual void Unk_88();
+	virtual void Unk_89();
+	virtual void Unk_8A();
+	virtual void Unk_8B();
+	virtual void Unk_8C();
+	virtual void Unk_8D();
+	virtual void Unk_8E();
+	virtual void Unk_8F();
+
+	BaseProcess	* process;	// 138
+	float		unk140;		// 140
+	u32			pad144;		// 144
+};
+
+static_assert(sizeof(MobileObject) == 0x148);
+
+// 318
+class Actor : public MobileObject
+{
+public:
+	virtual ~Actor();
+
+	virtual void Unk_90();
+	virtual void Unk_91();
+	virtual void Unk_92();
+	virtual void Unk_93();
+	virtual void Unk_94();
+	virtual void Unk_95();
+	virtual void Unk_96();
+	virtual void Unk_97();
+	virtual void Unk_98();
+	virtual void Unk_99();
+	virtual void Unk_9A();
+	virtual void Unk_9B();
+	virtual void Unk_9C();
+	virtual void Unk_9D();
+	virtual void Unk_9E();
+	virtual void Unk_9F();
+	virtual void Unk_A0();
+	virtual void Unk_A1();
+	virtual void Unk_A2();
+	virtual void Unk_A3();
+	virtual void Unk_A4();
+	virtual void Unk_A5();
+	virtual void Unk_A6();
+	virtual void Unk_A7();
+	virtual void Unk_A8();
+	virtual void Unk_A9();
+	virtual void Unk_AA();
+	virtual void Unk_AB();
+	virtual void Unk_AC();
+	virtual void Unk_AD();
+	virtual void Unk_AE();
+	virtual void Unk_AF();
+	virtual void Unk_B0();
+	virtual void Unk_B1();
+	virtual void Unk_B2();
+	virtual void Unk_B3();
+	virtual void Unk_B4();
+	virtual void Unk_B5();
+	virtual void Unk_B6();
+	virtual void Unk_B7();
+	virtual void Unk_B8();
+	virtual void Unk_B9();
+	virtual void Unk_BA();
+	virtual void Unk_BB();
+	virtual void Unk_BC();
+	virtual void Unk_BD();
+	virtual void Unk_BE();
+	virtual void Unk_BF();
+	virtual void Unk_C0();
+	virtual void Unk_C1();
+	virtual void Unk_C2();
+	virtual void Unk_C3();
+	virtual void Unk_C4();
+	virtual void Unk_C5();
+	virtual void Unk_C6();
+	virtual void Unk_C7();
+	virtual void Unk_C8();
+	virtual void Unk_C9();
+	virtual void Unk_CA();
+	virtual void Unk_CB();
+	virtual void Unk_CC();
+	virtual void Unk_CD();
+	virtual void Unk_CE();
+	virtual void Unk_CF();
+	virtual void Unk_D0();
+	virtual void Unk_D1();
+	virtual void Unk_D2();
+	virtual void Unk_D3();
+	virtual void Unk_D4();
+	virtual void Unk_D5();
+	virtual void Unk_D6();
+	virtual void Unk_D7();
+	virtual void Unk_D8();
+	virtual void Unk_D9();
+	virtual void Unk_DA();
+	virtual void Unk_DB();
+	virtual void Unk_DC();
+	virtual void Unk_DD();
+	virtual void Unk_DE();
+	virtual void Unk_DF();
+	virtual void Unk_E0();
+	virtual void Unk_E1();
+	virtual void Unk_E2();
+	virtual void Unk_E3();
+	virtual void Unk_E4();
+	virtual void Unk_E5();
+	virtual void Unk_E6();
+	virtual void Unk_E7();
+	virtual void Unk_E8();
+	virtual void Unk_E9();
+	virtual void Unk_EA();
+	virtual void Unk_EB();
+	virtual void Unk_EC();
+	virtual void Unk_ED();
+	virtual void Unk_EE();
+	virtual void Unk_EF();
+	virtual void Unk_F0();
+	virtual void Unk_F1();
+	virtual void Unk_F2();
+	virtual void Unk_F3();
+	virtual void Unk_F4();
+	virtual void Unk_F5();
+	virtual void Unk_F6();
+	virtual void Unk_F7();
+	virtual void Unk_F8();
+	virtual void Unk_F9();
+	virtual void Unk_FA();
+	virtual void Unk_FB();
+	virtual void Unk_FC();
+	virtual void Unk_FD();
+	virtual void Unk_FE();
+	virtual void Unk_FF();
+	virtual void Unk_100();
+	virtual void Unk_101();
+	virtual void Unk_102();
+	virtual void Unk_103();
+	virtual void Unk_104();
+	virtual void Unk_105();
+	virtual void Unk_106();
+	virtual void Unk_107();
+	virtual void Unk_108();
+	virtual void Unk_109();
+	virtual void Unk_10A();
+	virtual void Unk_10B();
+	virtual void Unk_10C();
+	virtual void Unk_10D();
+	virtual void Unk_10E();
+	virtual void Unk_10F();
+
+	u64	unk148[(0x318 - 0x148) / 8];
+};
+
+static_assert(sizeof(Actor) == 0x318);
+
+// 328
+class Character : public Actor
+{
+public:
+	virtual ~Character();
+
+	u64	unk318[(0x328 - 0x318) / 8];
+};
+
+static_assert(sizeof(Character) == 0x328);
+
+// B70
+class PlayerCharacter : public Character
+{
+public:
+	virtual ~PlayerCharacter();
+
+	u64	unk328[(0xB70 - 0x328) / 8];
+};
+
+static_assert(sizeof(PlayerCharacter) == 0xB70);
